1952afc689974aaa793c0688fa0fd08ea2da68b4
Khyati Singh <142620584+human-in-tech@users.noreply.github.com>
2025-04-18 07:31:03 +0000

    learnt more :3

---
+++ basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// old JS vs new JS 
+"use strict" // treats all JS code as newer version of JS. not really needed no more. automatic.
+
+// avoid semi colons :3
+
+//  primitive datatypes
+let name = "string"
+let age = 34 // range for integer = 2^53 next is big int
+let boolean = true
+let meow = null // null = datatype AND a stand-alone value
+
+console.log(typeof meow) 
+// null vs undefined : null is representation of empty value. undefined is NOT assigning the value yet.
---
+++ basics/03_variables.js
@@ -0,0 +1,12 @@
+const accountID = 14453
+let accountEmail = "kts@gmail.com" // bro used string
+//this is the preferred way of declaring variable
+
+// var was used before. scope cant be used
+var accountPWD = "12345" 
+accountCity = "delhi" // a very bad way of declaring variables
+console.log(accountCity)
+let accountState // this will show us 'undefined'
+console.table([accountEmail,accountID,accountCity,accountState]) //allows us to prin
+
+// accountID = 2  this will give us a runtime error
